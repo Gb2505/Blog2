c753b464512160cf8d1067cd7743a4c01b0b1a74
PROGRAMAÇÃO EM PYTHON 2024.4 <51129532024.4@senacrjedu.local>
2025-02-10 21:44:31 -0300

    ProjetO

---
+++ esporte/apps.py
@@ -0,0 +1,10 @@
+from django.apps import AppConfig
+
+
+class EsporteConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'esporte'
+
+class ContatoConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'contato'
---
+++ esporte/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class Blog(models.Model):
+    apelido = models.CharField(max_length=100, null=False, blank=False)
+    email = models.EmailField(max_length=100, null=False, blank=False)
+    senha = models.CharField(max_length=50, null=False, blank=False)
+
+    def  __str__(self):
+        return self.apelido
+
+class Contato(models.Model):
+    nome = models.CharField(max_length=100, null=False, blank=False)
+    email = models.EmailField()
+    mensagem = models.TextField()
+
+    def __str__(self):
+        return self.nome
